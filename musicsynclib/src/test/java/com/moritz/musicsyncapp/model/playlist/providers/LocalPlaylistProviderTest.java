99ae33406ed8f4e8ad911585797a602c29ff1871
moritz <hi@moritz.me>
2021-11-16 20:45:50 +0100

    added test

---
+++ musicsynclib/src/test/java/com/moritz/musicsyncapp/model/playlist/providers/LocalPlaylistProviderTest.java
@@ -0,0 +1,12 @@
+package com.moritz.musicsyncapp.model.playlist.providers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalPlaylistProviderTest {
+
+    @Test
+    void getAllPlaylists() {
+    }
+}
